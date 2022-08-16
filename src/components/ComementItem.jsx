fe9dba9a9bfb405593cf998c09d8ecc9d592776f
Bojan Golic <bokigolic32@gmail.com>
2022-08-16 13:38:10 -0400

    edit and cancel works

--- src/components/ComementItem.jsx
+++ src/components/ComementItem.jsx
@@ -1,14 +1,35 @@
+import { useState } from "react";
+
 const CommentItem = (props) => {
   const item = props.item
-  return (
-    <div className="comment-item">
+
+  const [editMode, setEditMode] = useState(false);
+
+  let jsx = (
       <div>
         <span>{item.title}</span>
         <span>{item.comment}</span>
+        <button onClick={()=>{setEditMode(true)}}>Edit</button>
+        <button>Delete</button>
       </div>
-      <button>Edit</button>
-      <button>Delete</button>
+       );
+
+       if(editMode){
+        jsx = (
+          <div>
+            <input value={item.title}></input>
+            <input value={item.comment}></input>
+            <button>Save</button>
+        <button onClick={()=>{setEditMode(false)}}>Cancel</button>
+          </div>
+        )
+       }
+  
+
+  return (
+    <div className="comment-item">
+      {jsx}
     </div>
-  );
+  )
 };
 export default CommentItem;
